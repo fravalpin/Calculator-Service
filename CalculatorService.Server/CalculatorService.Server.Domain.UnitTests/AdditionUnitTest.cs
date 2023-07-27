966b73e75ad3a8b4639e23219a32b7400f6fc946
Francisco VALLE PINAR <fvallepinar@gmail.com>
2023-07-27 20:07:07 +0200

    Addition logic

---
+++ CalculatorService.Server/CalculatorService.Server.Domain.UnitTests/AdditionUnitTest.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CalculatorService.Server.Domain.UnitTests
+{
+    public class AdditionUnitTest
+    {
+
+        [Fact]
+        public void AdditionNotOperands()
+        {
+            //setup
+            double[] operands = null;
+            Addition addition = new(operands);
+
+            //act
+            Action act = () => addition.Calculate();
+
+            //asserts
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void AdditionJustOneOperand()
+        {
+            //setup
+            double[] operands = new double[] { 1 };
+            Addition addition = new(operands);
+
+            //act
+            Action act = () => addition.Calculate();
+
+            //asserts
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void AdditionIntegers()
+        {
+            //setup
+            double[] operands = new double[] { 1, 1 };
+            Addition addition = new(operands);
+
+            //act
+            double result = addition.Calculate();
+
+            //asserts
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void AdditionDoubles()
+        {
+            //setup
+            double[] operands = new double[] { 1.5, 1.5 };
+            Addition addition = new(operands);
+
+            //act
+            double result = addition.Calculate();
+
+            //asserts
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void AdditionNegatives()
+        {
+            //setup
+            double[] operands = new double[] { -1, -1 };
+            Addition addition = new(operands);
+
+            //act
+            double result = addition.Calculate();
+
+            //asserts
+            result.Should().Be(-2);
+        }
+
+    }
+}
---
+++ CalculatorService.Server/CalculatorService.Server.Domain/Addition.cs
@@ -0,0 +1,22 @@
+﻿namespace CalculatorService.Server.Domain
+{
+    public class Addition
+    {
+        private double[] _operands;
+
+        public Addition(double[] operands)
+        {
+            _operands = operands;
+        }
+
+        public double Calculate()
+        {
+            if(_operands == null )  
+                throw new ArgumentNullException("The request should include at least two numeric operands to add"); 
+            if(_operands.Length < 2)
+                throw new ArgumentException("The request should include at least two numeric operands to add"); 
+
+            return _operands.Sum();
+        }
+    }
+}
