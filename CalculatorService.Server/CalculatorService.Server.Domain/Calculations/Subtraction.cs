049ee6cbe6cc3d3d7e12ede2611a76967f09f8c2
Francisco VALLE PINAR <fvallepinar@gmail.com>
2023-07-30 21:43:29 +0200

    Comment added to explain doubt in the subtraction

--- CalculatorService.Server/CalculatorService.Server.Domain/Calculations/Subtraction.cs
+++ CalculatorService.Server/CalculatorService.Server.Domain/Calculations/Subtraction.cs
@@ -7,6 +7,8 @@
 
         public Subtraction(double minuend, double subtrahend)
         {
+            //Doubt. According to the example the minuend is 3 and the subtrahend is -7 and the difference is -4. So I understand that you have to add to get this result.
+            //I don't know if it was an error in the PDF or I didn't understand it correctly.
             Value = minuend + subtrahend;
             _minuend = minuend;
             _subtrahend = subtrahend;
